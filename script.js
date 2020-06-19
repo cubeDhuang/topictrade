4e3cb4f2bac3d5b22a41ea1f18cf24c494bb9574
cubedhuang <99danielh@gmail.com>
2020-06-18 21:22:30 -0400

    create the thing

---
+++ script.js
@@ -0,0 +1,55 @@
+const app = new Vue({
+	el: "#app",
+
+	created() {
+		this.probabilities = this.votes.map(cur => Math.sqrt(cur + 1));
+	},
+
+	data: {
+		topics: [
+			"Charter Schools",
+			"Persian Gulf",
+			"Belt and Road Initiative",
+			"Universal Basic Income",
+			"Pharmaceutical Price Controls",
+			"Offensive Cyber Operations",
+			"Offshore Balancing",
+		],
+		votes: [
+			1,
+			10,
+			7,
+			13,
+			0,
+			1,
+			1
+		],
+		probabilities: [],
+		output: "Click the button to get a topic!",
+	},
+
+	computed: {
+		pTotal() {
+			return this.probabilities.reduce((prev, cur) => !!prev || prev === 0 ? prev + cur : cur);
+		}
+	},
+
+	methods: {
+		newTopic() {
+			let chosen = Math.random() * this.pTotal;
+
+			let added = 0;
+
+			for (let i = 0; i < this.probabilities.length; i++) {
+				added += this.probabilities[i];
+
+				if (chosen < added) {
+					this.output = this.topics[i];
+					return;
+				}
+			}
+
+			this.output = "Something went wrong - please roll again.";
+		}
+	}
+});
